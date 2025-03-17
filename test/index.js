29ac449a224e93fb5477b14a6269a77ba2548935
odootest2023 <odootest2023@163.com>
2025-03-17 21:18:56 +0800

    support template-json

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from 'node:fs/promises'
 
-import { office2json, json2office } from '../index.js'
+import { office2json, json2office, template } from '../index.js'
 
 const files = ['test.docx', 'test.xlsx', 'test.pptx']
 
@@ -17,3 +17,23 @@ for (const file of files) {
 
   await writeFile(`output-${file}`, buffer)
 }
+
+const json = {
+  a: {
+    b: {
+      c: [
+        {
+          d: 'ok {{value1}}  ',
+        },
+      ],
+    },
+    e: '{{value1}} {{value2}}',
+  },
+}
+
+const result = template(json, {
+  value1: 'hello',
+  value2: 'world',
+})
+
+console.dir({ json, result }, { depth: null })
--- test/template-json.js
+++ test/template-json.js
@@ -1,21 +1,24 @@
-import { template } from '../index.js'
-
-const json = {
-  a: {
-    b: {
-      c: [
-        {
-          d: 'ok {{value1}}  ',
-        },
-      ],
-    },
-    e: '{{value1}} {{value2}}',
-  },
+if (process.argv.length < 4) {
+  console.log("node ./template-json.js ./xxx.[docx|xlsx|pptx] 'json_string'")
+  process.exit()
 }
 
-const result = template(json, {
-  value1: 'hello',
-  value2: 'world',
-})
+const from_path = process.argv[2]
+const to_path = 'output-template-' + from_path
+const data = JSON.parse(process.argv[3])
 
-console.dir({ json, result }, { depth: null })
+import { readFile, writeFile } from 'node:fs/promises'
+
+import { office2json, json2office, template } from '../index.js'
+
+const from = await readFile(from_path)
+
+const json = await office2json(from)
+
+const new_json = template(json, data)
+
+const to = await json2office(new_json)
+
+const buffer = Buffer.from(to, 'base64')
+
+await writeFile(to_path, buffer)
